9b603fb848aa2508ff35c1674ccbdd3636bd083d
DIMANVAZ <DIMANVAZ04@GMAIL.COM>
2022-12-24 17:37:43 +0300

    update index.ts and create new scripts in package.json

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 // для создания Аполло сервера
 import {ApolloServer} from "apollo-server";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core"; //подключить Playground вместо ApolloStudio -1
 import {schema} from './schema';
 import {context} from "./context";
-
-import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core"; //подключить Playground вместо ApolloStudio -1
 
 const server = new ApolloServer({
     schema,
     context,
+    introspection: true,                                      // 1
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground()], //подключить Playground вместо ApolloStudio -2
 })
 
-const port = 3000;
+const port = process.env.port || 3000;
 server.listen({port}).then(({url}) => console.log('🚀 apollo server listening on',url ));
